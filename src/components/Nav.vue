bc2ad911672465f85e3b70514ca5d5034810380f
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-09-19 23:40:45 -0700

    feat: sign out

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import DarkLightModeToggle from "./common/DarkLightModeToggle.vue";
 import { RouterLink } from "vue-router";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { firebaseApp } from "../firebase";
+import { ref } from "vue";
+
+const auth = getAuth(firebaseApp);
+
+const user = ref(auth.currentUser);
+
+onAuthStateChanged(auth, (currentUser) => {
+    user.value = currentUser;
+});
+
+const signOut = async () => {
+    await auth.signOut();
+};
 </script>
 
 <template>
@@ -8,7 +23,9 @@ import { RouterLink } from "vue-router";
         class="h-16 flex justify-between items-center px-8 border-b-[1px] dark:border-zinc-800"
     >
         <h1 class="text-zinc-800 dark:text-zinc-100 text-sm font-bold">
-            <RouterLink to="/"> Punch Clock </RouterLink>
+            <RouterLink to="/">
+                Punch<span class="text-amber-400">Clock</span>
+            </RouterLink>
         </h1>
 
         <div class="flex items-center justify-end">
@@ -16,12 +33,30 @@ import { RouterLink } from "vue-router";
             <span
                 class="border-r-[1px] h-4 m-4 w-0 dark:border-zinc-600 border-zinc-300"
             ></span>
+
             <RouterLink
                 class="bg-zinc-900 hover:bg-zinc-700 text-white px-3 py-2 rounded-md text-sm shadow-sm"
                 to="/signin"
+                v-if="!user"
             >
                 Sign In
             </RouterLink>
+    
+            <RouterLink
+                class="bg-zinc-900 hover:bg-zinc-700 text-white px-3 py-2 rounded-md text-sm shadow-sm ml-4"
+                to="/signup"
+                v-if="!user"
+            >
+                Sign Up
+            </RouterLink>
+
+            <button
+                class="bg-zinc-900 hover:bg-zinc-700 text-white px-3 py-2 rounded-md text-sm shadow-sm"
+                v-if="user"
+                @click="signOut"
+            >
+                Sign Out
+            </button>
         </div>
     </nav>
 </template>
