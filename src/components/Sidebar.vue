77bdaf931efaaf907d0a27c078a089162e2f9021
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-09-27 18:36:30 -0700

    feat: Sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,25 @@
+import React from "react";
+import { BsGearFill, BsChatDotsFill } from "react-icons/bs";
+import { GiReactor } from "react-icons/gi";
+
+import useAuth from "../hooks/useAuth";
+
+import SideBarItem from "./common/SideBarItem";
+
+const SideBar = () => {
+  const { user } = useAuth();
+
+  return (
+
+<script setup lang="ts">
+import SidebarItem from './SidebarItem.vue';
+import ChatIcon from './icons/ChatIcon.vue';
+
+
+</script>
+
+<template>
+    <div class="z-20 flex h-full w-16 shrink-0 flex-col dark:bg-zinc-950 border-r-[1px] dark:border-zinc-800">
+        <SidebarItem :icon="ChatIcon" tooltip="Chat" class="text-accent-500" />
+    </div>
+</template>
---
+++ src/components/SidebarItem.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps(['icon', 'tooltip', 'onClick']);
+</script>
+
+<template>
+    <div @click="onClick()"
+        class="group relative mx-auto my-2 flex h-12 w-12 cursor-pointer items-center justify-center rounded-3xl bg-zinc-50 text-accent-400 shadow-lg transition-all duration-200 ease-linear hover:rounded-xl hover:bg-accent-400 hover:text-white dark:bg-zinc-800 dark:hover:bg-accent-400">
+        <component :is="icon" />
+        <span
+            class="absolute left-14 m-2 w-auto min-w-max origin-left scale-0 select-none rounded-md bg-zinc-50 py-2 px-4 text-xs font-bold text-zinc-600 shadow-md transition-all group-hover:scale-100 dark:bg-zinc-800 dark:text-zinc-300">
+            {{tooltip}}
+        </span>
+    </div>
+</template>
