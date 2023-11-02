a28b37c2fe7f62c6043a98208d8afb3568005de0
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-11-02 09:42:32 -0700

    add prop class

--- src/components/common/Input.vue
+++ src/components/common/Input.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-defineProps(["value", "type", "placeholder"]);
+defineProps(["value", "type", "placeholder", "class"]);
 
 defineEmits(["update:value"]);
 </script>
@@ -15,5 +15,6 @@ defineEmits(["update:value"]);
         spellcheck="false"
         autocomplete="off"
         class="my-3 w-full rounded-lg bg-white py-3 px-6 text-sm text-zinc-900 outline-none dark:bg-zinc-900/50 dark:text-zinc-100"
+        :class="class"
     />
 </template>
