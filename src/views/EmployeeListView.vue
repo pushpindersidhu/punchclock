c6ca06189da1e9189a22e2cf707355c0c8a92d1f
Pushpinder Sidhu <pushpindersidhu1984@gmail.com>
2023-11-08 01:50:15 -0800

    Update EmployeeListView.vue with dark mode support

--- src/views/EmployeeListView.vue
+++ src/views/EmployeeListView.vue
@@ -28,20 +28,20 @@ onBeforeUnmount(unsubscribe);
             </RouterLink>
         </div>
         <div class="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-2 lg:gap-8 gap-4 mt-8">
-            <div v-for="emp in employees" class="bg-gray-100 p-8 flex flex-col items-center justify-center rounded-2xl">
+            <div v-for="emp in employees" class="bg-gray-100 dark:bg-zinc-900 p-8 flex flex-col items-center justify-center rounded-2xl">
                 <img :src="emp.photo" :alt="emp.name" class="w-40 h-40 rounded-full object-cover" v-if="emp.photo">
-                <Icon icon="material-symbols:person" class="w-40 h-40 rounded-full bg-white text-zinc-200 p-8" v-else />
-                <p class="mt-4">
+                <Icon icon="material-symbols:person" class="w-40 h-40 rounded-full bg-white dark:bg-zinc-950 dark:text-zinc-800 text-zinc-200 p-8" v-else />
+                <p class="mt-4 dark:text-zinc-200">
                     {{ emp.name }}
                 </p>
 
-                <a class="mt-2 text-sm text-gray-500 hover:text-gray-900 cursor-pointer text-ellipsis"
+                <a class="mt-2 text-sm text-gray-500 dark:text-zinc-400 hover:text-gray-900 cursor-pointer text-ellipsis"
                     :href="'mailto:' + emp.email">
                     <Icon icon="ic:baseline-email" class="inline-block w-4 h-4 mr-2" />
                     {{ emp.email }}
                 </a>
 
-                <a class="mt-2 text-sm text-gray-500 hover:text-gray-900 cursor-pointer" :href="'tel:' + emp.phone">
+                <a class="mt-2 text-sm text-gray-500 dark:text-zinc-400 hover:text-gray-900 cursor-pointer" :href="'tel:' + emp.phone">
                     <Icon icon="ic:baseline-phone" class="inline-block w-4 h-4 mr-2
                     " />
                     {{ emp.phone }}
